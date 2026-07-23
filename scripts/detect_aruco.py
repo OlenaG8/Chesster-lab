34835fc200d7f166c91e77dd02aad304ccae605c
OlenaG8 <gaponenkoolena01@gmail.com>
2026-07-23 12:11:39 +0200

    Change function to return aruco corners

--- scripts/detect_aruco.py
+++ scripts/detect_aruco.py
@@ -7,7 +7,7 @@ def generate_aruco(marker_id):
     marker_image = cv2.aruco.generateImageMarker(aruco_dict, marker_id, marker_size)
     cv2.imwrite(f'../aruco-markers/marker-{marker_id}.png', marker_image)
 
-def get_chessboard_state(frame):
+def get_aruco_corners(frame):
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
     parameters = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
@@ -16,39 +16,11 @@ def get_chessboard_state(frame):
     corners, ids, rejected = detector.detectMarkers(gray)
 
     if ids is not None and len(ids) == 4:
-        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
         sorted_corners = [c for _, c in sorted(zip(ids.flatten(), corners))]
+        return sorted_corners, ids
+    return None, None
 
-        src = np.array([[0, 0], [8, 0], [8, 8], [0, 8]], dtype="float32")
-        dst = np.array([c[0][0] for c in sorted_corners], dtype="float32")
-        H = cv2.getPerspectiveTransform(src, dst)
-
-        src_grid = np.array([[[x, y] for x in range(9)] for y in range(9)], dtype=np.float32)
-        dst_grid = cv2.perspectiveTransform(src_grid.reshape(-1, 1, 2), H).reshape(9, 9, 2)
-
-        chessboard_state = {}
-        for r in range(8):
-            for c in range(8):
-                tl = dst_grid[r, c].tolist()
-                tr = dst_grid[r, c + 1].tolist()
-                br = dst_grid[r + 1, c + 1].tolist()
-                bl = dst_grid[r + 1, c].tolist()
-                chessboard_state[(r, c)] = [tl, tr, br, bl]
-
-        files = 'abcdefgh'
-        ranks = '87654321'
-        squares_std = {}
-        for (r_disp, c_disp), poly in chessboard_state.items():
-            r_std, c_std = r_disp, c_disp
-            file_letter = files[c_std]
-            rank_char = ranks[r_std]
-            squares_std[f"{file_letter}{rank_char}"] = poly
-
-        return squares_std
-    return None
-
-
-def draw_grid_in_perspective(frame, sorted_corners):
+def draw_grid(frame, sorted_corners):
     size = 800
     ideal_corners = np.array([[0, 0], [size, 0], [size, size], [0, size]], dtype="float32")
     image_corners = np.array([c[0][0] for c in sorted_corners], dtype="float32")
@@ -90,7 +62,7 @@ def detect_aruco_live():
         if ids is not None and len(ids) == 4:
             cv2.aruco.drawDetectedMarkers(frame, corners, ids)
             sorted_corners = [c for _, c in sorted(zip(ids.flatten(), corners))]
-            draw_grid_in_perspective(frame, sorted_corners)
+            draw_grid(frame, sorted_corners)
 
         cv2.imshow('Podglad z kamery', frame)
 
