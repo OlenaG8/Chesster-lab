5a170782ff667d0de9f746393bc034786d7bba4a
OlenaG8 <gaponenkoolena01@gmail.com>
2026-07-29 16:07:30 +0200

    Add a config file

---
+++ project/config.py
@@ -0,0 +1,6 @@
+CAM_INDEX = 4
+BOARD_SIZE = 800
+CELL_SIZE = BOARD_SIZE // 8
+
+ENGINE_PATH = r"/home/olena/Downloads/stockfish-ubuntu-x86-64/stockfish/stockfish-ubuntu-x86-64"
+CNN_MODEL_PATH = "models/chess_piece_cnn.pt"
