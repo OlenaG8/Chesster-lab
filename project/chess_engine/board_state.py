acc98fcdd66a2c15fe067b299500d6ce4a9caca6
OlenaG8 <gaponenkoolena01@gmail.com>
2026-09-08 14:49:38 +0200

    Add functions to display the state of the board

--- project/chess_engine/board_state.py
+++ project/chess_engine/board_state.py
@@ -1,4 +1,11 @@
+import io
+
+import cairosvg
 import chess
+import chess.svg
+import cv2
+import numpy as np
+from PIL import Image
 
 
 class BoardState:
@@ -15,3 +22,14 @@ class BoardState:
     def push(self,uci):
         move = chess.Move.from_uci(uci)
         self.board.push(move)
+
+    def show_board(self, last_move=None):
+        svg = chess.svg.board(self.board, lastmove=last_move, coordinates=True, size=450)
+        png_data = cairosvg.svg2png(bytestring=svg.encode('utf-8'))
+        img = Image.open(io.BytesIO(png_data))
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        cv2.imshow("Board State", img_cv)
+        cv2.waitKey(1)
+
+    def display(self):
+        print(self.board.unicode())
