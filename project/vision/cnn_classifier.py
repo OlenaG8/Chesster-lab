34598570be05d92d0c6d600ac0914b670c40cb12
OlenaG8 <gaponenkoolena01@gmail.com>
2026-09-08 14:55:30 +0200

    Add transform normalization

--- project/vision/cnn_classifier.py
+++ project/vision/cnn_classifier.py
@@ -2,22 +2,30 @@ import os
 import torch
 import torchvision.transforms as transforms
 
-class ChessCNN:
+from project.models.cnn.model import ChessCNN
+
+class ChessCNNClassifier:
     def __init__(self, model_path):
         if not os.path.exists(model_path):
-            raise FileNotFoundError(f'Model {model_path} not found')
+            raise FileNotFoundError(f"Model {model_path} not found")
 
-        size = os.path.getsize(model_path)
-        if size == 0:
-            raise RuntimeError(f'Model {model_path} is empty')
+        if os.path.getsize(model_path) == 0:
+            raise RuntimeError(f"Model {model_path} is empty")
 
-        self.model = torch.load(model_path, map_location="cpu")
+        self.model = ChessCNN()
+        state_dict = torch.load(model_path, map_location="cpu", weights_only=False)
+
+        self.model.load_state_dict(state_dict)
         self.model.eval()
 
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.Resize((64,64)),
+            transforms.Resize((64, 64)),
             transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5]
+            )
         ])
 
     def predict(self, square):
@@ -28,4 +36,5 @@ class ChessCNN:
             output = self.model(x)
 
         cls = output.argmax(1)
+
         return cls.item()
