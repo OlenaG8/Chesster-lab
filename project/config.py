eb6d40a4b01ccca45ef40054b2b8c1dcd0f6e5cb
OlenaG8 <gaponenkoolena01@gmail.com>
2026-09-23 15:21:28 +0200

    Move variables

--- project/config.py
+++ project/config.py
@@ -3,7 +3,9 @@ BOARD_SIZE = 800
 CELL_SIZE = BOARD_SIZE // 8
 
 MOTION_THRESHOLD = 500
-DELAY = 1
+MAX_BOARD_READ_TRIALS = 100
+DELAY = 1.5
+MAX_CACHED_FRAMES = 30
 
 ENGINE_PATH = r"/home/olena/Downloads/stockfish-ubuntu-x86-64/stockfish/stockfish-ubuntu-x86-64"
 CNN_MODEL_PATH = r"/home/olena/Chesster-lab/project/models/cnn/chess_piece_cnn_v4.pt"
