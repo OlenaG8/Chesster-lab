807af9947d714e8aa6dee6f48d8f75225348c40c
OlenaG8 <gaponenkoolena01@gmail.com>
2026-09-23 11:38:14 +0200

    Make delay shorter

--- project/config.py
+++ project/config.py
@@ -3,7 +3,7 @@ BOARD_SIZE = 800
 CELL_SIZE = BOARD_SIZE // 8
 
 MOTION_THRESHOLD = 500
-DELAY = 2
+DELAY = 1
 
 ENGINE_PATH = r"/home/olena/Downloads/stockfish-ubuntu-x86-64/stockfish/stockfish-ubuntu-x86-64"
 CNN_MODEL_PATH = r"/home/olena/Chesster-lab/project/models/cnn/chess_piece_cnn_v4.pt"
